4ca170d47ad27ee6dd1a69a795f87da5abdf1c22
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-07-09 14:50:07 -0600

    JS ch3 example
    finished JS and am experiencing some bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* The script is place inside an immediately invoked function expression 
+/* The script is placed inside an immediately invoked function expression 
 which helps protect the scope of variables */
 
 (function() {
@@ -8,8 +8,10 @@ which helps protect the scope of variables */
     // Create a hotel object using object literal syntax
     let hotel = {
         name: 'Park',
-        roomRate: 240, //Amount in dollars
-        discount: 15, // Percentage discount
+        roomRate: 240,
+        //Amount in dollars
+        discount: 15, 
+        // Percentage discount
         offerPrice: function() {
             let offerRate = this.roomRate * ((100 - this.discount) / 100);
             return offerRate;
@@ -17,42 +19,59 @@ which helps protect the scope of variables */
     }
 
     // Write out the hotel name, standard rate, and the special rate
-    let hotelName, roomRate, specialRate;   //Declare variables
-    
-    hotelName = document.getElemenyById('hotelName');   // Get elements
+    let hotelName, roomRate, specialRate;   
+    //Declare variables
+
+    // Get elements
+    hotelName = document.getElemenyById('hotelName');   
     roomRate = document.getElementById('roomRate');
     specialRate = document.getElementById('specialRate');
-
-    hotelName.textContent = hotel.name;     // Write hotel name
-    roomRate.textContent = '$' + hotel.roomRate.toFixed(2);     // Write room rate
-    specialRate.textContent = '$' + hotel.offerPrice();     // Write offer price
+    
+    // Write hotel name
+    hotelName.textContent = hotel.name;    
+    // Write room rate
+    roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+    // Write offer price     
+    specialRate.textContent = '$' + hotel.offerPrice();     
 
     // PART TWO: CALCULATE AND WRITE OUT THE EXPIRY DETAILS FOR THE OFFER
-    let expiryMsg;  // Message displayed to users
-    let today;      // Today's date
-    let elEnds;     // The element that shows the message about the offer ending
+    let expiryMsg;  
+    // Message displayed to users
+    let today;      
+    // Today's date
+    let elEnds;     
+    // The element that shows the message about the offer ending
 
     function offerExpires(today) {
         // Declare variables within the function for local scope
         let weekFromToday, day, date, month, year, dayNames, monthNames;
+
         // Add 7 days time (added in milliseconds)
         weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
         // Create arrays to hold the names of days / months
         dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
             'Saturday'];
         monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
             'August', 'September', 'October', 'November', 'December'];
+
         // Collect the parts of the date to show on the page
         day = dayNames[weekFromToday.getDay()];
         date = weekFromToday.getDate();
         month = monthNames[weekFromToday.getMonth()];
         year = weekFromToday.getFullYear();
+
         // Create the message
         expiryMsg = 'Offer expires next ';
         expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
         return expiryMsg;
     }
 
-    
-}
-)
+    today = new Date();     
+    // Put today's date in variable
+    elEnds = document.getElementById('offerEnds');      
+    // Get the offerEnds element
+    elEnds.innerHTML = offerExpires(today);
+
+    // Finish the immediately invoked expression
+}())
