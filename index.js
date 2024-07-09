84c29c60742fa6f032269678d652a29fb35fc551
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-07-09 14:29:53 -0600

    JS ch3 example
    added part two JS

--- index.js
+++ index.js
@@ -33,7 +33,26 @@ which helps protect the scope of variables */
     let elEnds;     // The element that shows the message about the offer ending
 
     function offerExpires(today) {
-        
+        // Declare variables within the function for local scope
+        let weekFromToday, day, date, month, year, dayNames, monthNames;
+        // Add 7 days time (added in milliseconds)
+        weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+        // Create arrays to hold the names of days / months
+        dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
+            'Saturday'];
+        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+            'August', 'September', 'October', 'November', 'December'];
+        // Collect the parts of the date to show on the page
+        day = dayNames[weekFromToday.getDay()];
+        date = weekFromToday.getDate();
+        month = monthNames[weekFromToday.getMonth()];
+        year = weekFromToday.getFullYear();
+        // Create the message
+        expiryMsg = 'Offer expires next ';
+        expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+        return expiryMsg;
     }
+
+    
 }
 )
