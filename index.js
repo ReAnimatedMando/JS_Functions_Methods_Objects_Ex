29df85401818739816c0f55fa63d42d1365e176f
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-07-10 10:50:42 -0600

    JS ch3 example
    fixed js but still not returning expiry message

--- index.js
+++ index.js
@@ -49,6 +49,28 @@
         // Add 7 days time (added in milliseconds)
         weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-        
+        // Create arrays to hold the names of days/months
+        dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        // Collect the parts of the date to show on the page
+        day = dayNames[weekFromToday.getDay()];
+        date = weekFromToday.getDate();
+        month = monthName[weekFromToday.getMonth()];
+        year = weekFromToday.getFullYear();
+
+        // Create the message
+        expiryMsg = 'Offer expires next ';
+        expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+        return expiryMsg;
     }
-})
+
+    today = new Date();
+    // Put today's date in variable
+    elEnds = document.getElementById('offerEnds');
+    // Get the offerEnds element
+    elEnds.innerHTML = offerExpires(today);
+    // Add the expiry message
+
+    // Finish the immediately invoked function expression
+}());
