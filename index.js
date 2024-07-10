1d63fe62cd410612507beeecd3133e38ed864fae
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-07-10 10:54:01 -0600

    JS ch3 example
    fixed variable declarations, and some spelling errors to finally get expiry message returned

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-/* The script is placed inside an immediately invoked function
-    which helps protect the scope of variables */
+/* The script is placed inside an immediately invoked function expression
+   which helps protect the scope of variables */
 
-(function() {
+   (function() {
 
     // PART ONE: CREATE HOTEL OBJECT AND WRITE OUT THE OFFER DETAILS
 
     // Create a hotel object using object literal syntax
-    let hotel =  {
+    var hotel = {
         name: 'Park',
         roomRate: 240,
         // Amount in dollars
         discount: 15,
         // Percentage discount
         offerPrice: function() {
-            let offerRate = this.roomRate * ((100 - this.discount) / 100);
+            var offerRate = this.roomRate * ((100 - this.discount) / 100);
             return offerRate;
         }
     };
 
     // Write out the hotel name, standard rate, and the special rate
-    let hotelName, roomRate, specialRate;
+    var hotelName, roomRate, specialRate;
     // Declare variables
 
     hotelName = document.getElementById('hotelName');
@@ -35,28 +35,28 @@
     // Write offer price
 
     // PART TWO: CALCULATE AND WRITE OUT THE EXPIRY DETAILS FOR THE OFFER
-    let expiryMsg;
+    var expiryMsg;
     // Message displayed to users
-    let today;
+    var today;
     // Today's date
-    let elEnds;
+    var elEnds;
     // The element that shows the message about the offer ending
 
     function offerExpires(today) {
         // Declare variables within the function for local scope
-        let weekFromToday, day, date, month, year, dayNames, monthNames;
+        var weekFromToday, day, date, month, year, dayNames, monthNames;
 
         // Add 7 days time (added in milliseconds)
         weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-        // Create arrays to hold the names of days/months
+        // Create arrays to hold the names of days / months
         dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         // Collect the parts of the date to show on the page
         day = dayNames[weekFromToday.getDay()];
         date = weekFromToday.getDate();
-        month = monthName[weekFromToday.getMonth()];
+        month = monthNames[weekFromToday.getMonth()];
         year = weekFromToday.getFullYear();
 
         // Create the message
